16eea356b7f8a2dc4166c25e89a1bcb348ba5801
yareeekc <268030883+yareeekc@users.noreply.github.com>
2026-08-09 02:56:42 +0300

    Update backend.sh for 0.0.5 ALPHA

--- backend.sh
+++ backend.sh
@@ -2,7 +2,7 @@
 echo 'Server Starting on 2345 Port'
 
 while true; do
-    nc -l 2345 > /tmp/request.txt
+    nc -l -k 2345 > /tmp/request.txt
 
     {
         if grep -q 'suspend' /tmp/request.txt; then
@@ -13,6 +13,20 @@ while true; do
             systemctl reboot
         elif grep -q 'ls' /tmp/request.txt; then
             ls
+        elif grep -q 'exec' /tmp/request.txt; then
+            RAW_CMD=$(grep 'exec' /tmp/request.txt | sed -E 's|.*GET /exec/([^ ]*) .*|\1|')
+
+            DECODED_CMD=$(echo "$RAW_CMD" | awk '{
+                gsub(/%20/, " "); gsub(/%2F/, "/"); gsub(/%3A/, ":");
+                gsub(/%2D/, "-"); gsub(/%3E/, ">"); gsub(/%3C/, "<");
+                print
+            }')
+
+            if [ -n "$DECODED_CMD" ]; then
+                eval "$DECODED_CMD"
+            else
+                echo 'usage: "exec <COMMAND>"'
+            fi
         fi
-    } | nc -l 2345 > /dev/null
+    } | nc -l -k 2345 > /dev/null
 done
