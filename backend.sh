48373203185c11158e85907ecaaca655a05ae94f
yareeekc <268030883+yareeekc@users.noreply.github.com>
2026-08-08 19:07:53 +0300

    Update backend.sh for 0.0.3 ALPHA

--- backend.sh
+++ backend.sh
@@ -2,13 +2,17 @@
 echo 'Server Starting on 2345 Port'
 
 while true; do
-    echo -e "HTTP/1.1 200 OK\r\nAccess-Control-Allow-Origin: *\r\nConnection: close\r\n\r\nOK" | nc -l 2345 > /tmp/request.txt
+    nc -l 2345 > /tmp/request.txt
 
-    if grep -q 'suspend' /tmp/request.txt; then
-        systemctl suspend
-    elif grep -q 'poweroff' /tmp/request.txt; then
-        systemctl poweroff
-    elif grep -q 'poweroff' /tmp/request.txt; then
-        systemctl reboot
-    fi
+    {
+        if grep -q 'suspend' /tmp/request.txt; then
+            systemctl suspend
+        elif grep -q 'poweroff' /tmp/request.txt; then
+            systemctl poweroff
+        elif grep -q 'poweroff' /tmp/request.txt; then
+            systemctl reboot
+        elif grep -q 'ls' /tmp/request.txt; then
+            ls
+        fi
+    } | nc -l 2345 > /dev/null
 done
